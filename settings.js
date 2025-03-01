922448aaafbccf736710a61f5c8369b43b9eb565
Kashfarmer <160387580+Kashfarmer@users.noreply.github.com>
2025-03-01 10:50:17 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "kashfarmer Bot",
+  botOwner: 'Ibrahim', // Your  name 
+  ownerNumber: '233240482472', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
